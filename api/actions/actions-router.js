f0e031ecf832b8ad71a2e0965deeed922a69b3c4
Karla Nieves <karlaxnieves@gmail.com>
2021-08-06 09:18:39 -0700

    the server is running

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,15 @@
-// Write your "actions" router here!
+const express = require('express')
+
+const Actions = require('./actions-model')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+    .then(actions => {
+        res.json(actions)
+    })
+    .catch(next)
+})
+
+module.exports = router;
