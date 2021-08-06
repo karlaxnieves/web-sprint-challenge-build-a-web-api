3d285c6f66ff69b2ef1e6261ae062403db4334aa
Karla Nieves <karlaxnieves@gmail.com>
2021-08-06 09:04:51 -0700

    index.js completed

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ Pull your server into this file and start it!
 */
 
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
+
+server.use(express.json())
+server.use(cors())
+
+server.listen(PORT, () => {
+    console.log('listening on', PORT)
+})
